0c2729d468bc6bd1677bd0c1bda088f75f5ce777
mazum <mazumder.abhishek@gmail.com>
2020-06-28 23:36:49 +1000

    feat: Kinesis Data Stream Demo- Status project
    https://www.youtube.com/watch?v=4-JmX6MIDDI

---
+++ KinesisDemo/KinesisStatus/Status.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Kinesis;
+using Amazon.Kinesis.Model;
+
+namespace KinesisStatus
+{
+    class Status
+    {
+        static async Task Main(string[] args)
+        {
+            const string accessKey = "<Access Key>";
+            const string secretKey = "<Secret Key>";
+
+            AmazonKinesisClient client = new AmazonKinesisClient(accessKey, secretKey, RegionEndpoint.USEast2);
+            var request = new ListShardsRequest()
+            {
+                StreamName = args[0]
+            };
+            while (true)
+            {
+                var response = await client.ListShardsAsync(request);
+                Console.Clear();
+                foreach (var shard in response.Shards)
+                {
+                    Console.WriteLine($"Shard {shard.ShardId}");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
