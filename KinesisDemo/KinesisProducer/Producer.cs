8df1277f4fd2314ad3b96bea6ad4c3bd6ca2c443
mazum <mazumder.abhishek@gmail.com>
2020-06-29 23:43:28 +1000

    feat: Kinesis Data Stream Demo- Prodiucer Project

---
+++ KinesisDemo/KinesisProducer/Producer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Amazon;
+using Amazon.Kinesis;
+using Amazon.Kinesis.Model;
+
+namespace KinesisProducer
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+            const string accessKey = "<Access Key>";
+            const string secretKey = "<Secret Key>";
+
+            AmazonKinesisClient client = new AmazonKinesisClient(accessKey, secretKey, RegionEndpoint.USEast2);
+
+            int i = 1;
+
+            var message = $"{args[1]}-{i++}";
+            var messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+            var messageBase64 = Convert.ToBase64String(messageBytes);
+            Console.WriteLine(messageBase64);
+            var request = new PutRecordRequest
+            {
+                Data = messageBase64
+            };
+        }
+    }
+}
